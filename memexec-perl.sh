a247d1948a2ba2108577112877fd78a36e5a8734
Root THC <root@proton.thc.org>
2025-03-26 21:23:19 +0000

    mipsel o32

--- memexec-perl.sh
+++ memexec-perl.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-memexec(){ perl '-e$^F=255;for(319,279,385,314){($f=syscall$_,$",0)>0&&last};open($o,">&=".$f);print$o(<STDIN>);exec{"/proc/$$/fd/$f"}X,@ARGV' -- "$@";}
+#  319 - x86_64
+#  279 - aarch64
+#  385 - armv6l
+# 4314 - mipsel n64
+# 4354 - mipsel o32
+memexec(){ perl '-e$^F=255;for(319,279,385,4314,4354){($f=syscall$_,$",0)>0&&last};open($o,">&=".$f);print$o(<STDIN>);exec{"/proc/$$/fd/$f"}X,@ARGV' -- "$@";}
 # memexec(){ perl '-efor(319,279){($f=syscall$_,$",1)>0&&last};open($o,">&=".$f);print$o(<STDIN>);exec{"/proc/$$/fd/$f"}X,@ARGV' -- "$@";}
 # Example: cat /usr/bin/id | memexec -u
 
